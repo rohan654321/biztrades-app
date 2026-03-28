bd8c250959ee52eea1428c9c000cd505b749a716
Sandhyaravoor <sandhyaravoor.25@gmail.com>
2026-03-28 13:08:19 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,17 @@ import "./globals.css"
 import { ClientLayout } from "./client-layout"
 import ConditionalLayout from "./conditional-layout"
 import { ReactQueryProvider } from "@/components/react-query-provider"
+import { Roboto } from "next/font/google"
 
 export const metadata: Metadata = {
   title: "Biz Trade Fairs",
   description: "Discover global trade fairs, connect with opportunities, and grow your business network.",
   generator: "Biz Trade Fairs",
 }
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+})
 
 export default function RootLayout({
   children,
@@ -18,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+       <body className={roboto.className}>
         <ClientLayout>
           <ConditionalLayout><ReactQueryProvider>{children}</ReactQueryProvider></ConditionalLayout>
         </ClientLayout>
